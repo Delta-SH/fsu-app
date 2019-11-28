8d260bd484db0ed139e7b872f532d3933c4f88bb
steven <huangying0621@gmail.com>
2019-11-28 16:30:37 +0800

    v1.1.0

--- src/pages/main/cameralist.js
+++ src/pages/main/cameralist.js
@@ -5,64 +5,52 @@ loader.define(function (require, exports, module) {
         _ticket,
         _id,
         _sources = [{
-                name: 'CCTV-1',
+                name: '测试#1',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8'
             },
             {
-                name: 'CCTV-2',
+                name: '测试#2',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv2.m3u8'
             },
             {
-                name: 'CCTV-3',
+                name: '测试#3',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv3hd.m3u8'
             },
             {
-                name: 'CCTV-4',
+                name: '测试#4',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv4.m3u8'
             },
             {
-                name: 'CCTV-5',
+                name: '测试#5',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8'
             },
             {
-                name: 'CCTV-6',
+                name: '测试#6',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8'
             },
             {
-                name: 'CCTV-7',
+                name: '测试#7',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv7.m3u8'
             },
             {
-                name: 'CCTV-8',
+                name: '测试#8',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv8hd.m3u8'
             },
             {
-                name: 'CCTV-9',
+                name: '测试#9',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv9.m3u8'
             },
             {
-                name: 'CCTV-10',
+                name: '测试#10',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv10.m3u8'
             },
             {
-                name: 'CCTV-11',
+                name: '测试#11',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv11.m3u8'
             },
             {
-                name: 'CCTV-12',
+                name: '测试#12',
                 src: 'http://ivi.bupt.edu.cn/hls/cctv12.m3u8'
-            },
-            {
-                name: 'CCTV-13',
-                src: 'http://ivi.bupt.edu.cn/hls/cctv13.m3u8'
-            },
-            {
-                name: 'CCTV-14',
-                src: 'http://ivi.bupt.edu.cn/hls/cctv14.m3u8'
-            },
-            {
-                name: 'CCTV-15',
-                src: 'http://ivi.bupt.edu.cn/hls/cctv15.m3u8'
             }
         ];
 
