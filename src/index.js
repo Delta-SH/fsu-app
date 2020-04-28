9a47c8a8fbb74b8eba13697771e8578dc451d5ac
steven <huangying0621@gmail.com>
2020-04-28 11:34:04 +0800

    增加字体图标

--- src/index.js
+++ src/index.js
@@ -164,7 +164,7 @@ bui.ready(function () {
   }
 });
 
-// BUI函数
+// 自定义函数
 function AppRequest(ip, user, token) {
   var _this = this;
   var _ip = ip;
@@ -1014,7 +1014,6 @@ function sayGoodbye() {
   }
 }
 
-// 自定义函数
 function getAlarmCls1(level) {
   if (level === $level.L1) return "level1";
   else if (level === $level.L2) return "level2";
@@ -1225,23 +1224,6 @@ function onInput(option) {
   });
 }
 
-function isNull(value) {
-  return typeof value == "undefined" || value === null;
-}
-
-function isEmpty(value, whitespace) {
-  return (whitespace || false) === true ? value.trim() === "" : value === "";
-}
-
-function isNullOrEmpty(value, whitespace) {
-  if (isNull(value) === true) return true;
-  return isEmpty(value, whitespace);
-}
-
-function isFunction(func) {
-  return func && typeof func == "function";
-}
-
 function getTimespan(start, end) {
   var from = bui.date.convert(start);
   var to = isNull(end) ? new Date() : bui.date.convert(end);
@@ -1253,40 +1235,3 @@ function getTimespan(start, end) {
   var seconds = parseInt(diff % 60);
   return String.format("{0}小时{1}分{2}秒", hours, minutes > 9 ? minutes : "0" + minutes, seconds > 9 ? seconds : "0" + seconds);
 }
-
-// 扩展方法
-String.format = function () {
-  if (arguments.length == 0) return null;
-
-  var str = arguments[0];
-  for (var i = 1; i < arguments.length; i++) {
-    var re = new RegExp("\\{" + (i - 1) + "\\}", "gm");
-    str = str.replace(re, arguments[i]);
-  }
-
-  return str;
-};
-String.prototype.startWith = function (value, ignoreCase) {
-  if (value == null || value == "" || this.length == 0 || value.length > this.length) {
-    return false;
-  }
-
-  ignoreCase = ignoreCase || false;
-  if (ignoreCase === true) {
-    return this.substr(0, value.length).toLowerCase() === value.toLowerCase();
-  }
-
-  return this.substr(0, value.length) === value;
-};
-String.prototype.endWith = function (value, ignoreCase) {
-  if (value == null || value == "" || this.length == 0 || value.length > this.length) {
-    return false;
-  }
-
-  ignoreCase = ignoreCase || false;
-  if (ignoreCase === true) {
-    return this.substr(this.length - value.length).toLowerCase() === value.toLowerCase();
-  }
-
-  return this.substr(this.length - value.length) === value;
-};
